a5bb168843c923939433c9e24f4dc1e8c9d9dc5e
Ewura Impraim <eimpraim@terpmail.umd.edu>
2023-04-04 14:07:54 -0400

    added something small

--- exercise.py
+++ exercise.py
@@ -1,3 +1,8 @@
 word = 'Hello'
 for l in range(len(word)):
     print(word[l])
+    
+    
+team = ["TJ","Ewura","Gordon","Simon","Alex"]
+for name in team:
+    print(f" {name} contributed to this exercise.")
