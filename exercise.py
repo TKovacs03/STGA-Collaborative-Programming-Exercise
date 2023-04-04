bef492746aed3a536f04425e7078b3f158849e44
simonhuynh11201 <shh@terpmail.umd.edu>
2023-04-04 18:39:57 -0400

    added something

--- exercise.py
+++ exercise.py
@@ -6,3 +6,7 @@ for l in range(len(word)):
 team = ["TJ","Ewura","Gordon","Simon","Alex"]
 for name in team:
     print(f" {name} contributed to this exercise.")
+
+name = "Simon"
+statement = f"{name} Completed Adding Something."
+print(statement)
