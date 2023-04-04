6db800bbaefffcce4fc69dcdd5a9b06b85ca3cf1
Gordon Brown <gbrown11@terpmail.umd.edu>
2023-04-04 19:09:22 -0400

    I added a dictionary

--- exercise.py
+++ exercise.py
@@ -6,3 +6,14 @@ for l in range(len(word)):
 team = ["TJ","Ewura","Gordon","Simon","Alex"]
 for name in team:
     print(f" {name} contributed to this exercise.")
+    
+
+stga_dict = {
+    "TJ" : "INST 327",
+    "Ewura": "INST 311",
+    "Gordon": "INST 326",
+    "Simon": "INST 408i",
+    "TJ": "INST 412"
+    
+}
+[print(f"{names} is taking {stga_dict[names]}") for names in stga_dict]
